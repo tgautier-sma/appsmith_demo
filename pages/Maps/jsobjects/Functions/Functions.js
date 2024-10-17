fcbe959c04a12b867f7fe538b5cb5bedab716fd2
Thierry GAUTIER <dev.gautiersa@gmail.com>
2024-10-17 15:17:34 +0000

    System generated commit, initial commit

---
+++ pages/Maps/jsobjects/Functions/Functions.js
@@ -0,0 +1,16 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	mapData () {
+		const demoData = [
+  { id:1,category: "Monuments", lat: 48.858253035502344, lng: 2.294501875358415, title: "Paris Tour Eiffel", attributes: [], description: "" },
+  { id:2,category: "Villes", lat: 51.507222, lng: -0.12755, title: "Londres", attributes: [], description: "" },
+  { id:3,category: "Villes", lat: 55.7517, lng: 37.6178, title: "Moscou", attributes: [], description: "" },
+  { id:4,category: "Villes", lat: 41.0128, lng: 28.9744, title: "Istanbul", attributes: [], description: "" },
+  { id:5,category: "Villes", lat: 41.9, lng: 12.5, title: "Rome", attributes: [], description: "" },
+  { id:6,category: "Villes", lat: 46.95, lng: 7.4458, title: "Bern", attributes: [], description: "" },
+];
+		return demoData;
+	},
+	
+}
